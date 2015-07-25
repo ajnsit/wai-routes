bfed5727ed17f86fb8b50b28e85858a32390c9bd
Anupam Jain <ajnsit@gmail.com>
2015-07-26 01:11:39 +0530

    Remove the StateT over IO for the RouteM Monad. Kept backwards compatibility.

--- src/Network/Wai/Middleware/Routes/Monad.hs
+++ src/Network/Wai/Middleware/Routes/Monad.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving, TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving, TypeFamilies, RankNTypes, DeriveFunctor #-}
 
 {- |
 Module      :  Network.Wai.Middleware.Routes.Monad
@@ -15,10 +15,12 @@ module Network.Wai.Middleware.Routes.Monad
     ( -- * Route Monad
       RouteM
       -- * Compose Routes
-    , defaultAction
     , middleware
     , route
+    , catchall
+    , defaultAction
       -- * Convert to Wai Application
+    , waiApp
     , toWaiApp
     )
     where
@@ -27,54 +29,67 @@ import Network.Wai
 import Network.Wai.Middleware.Routes.Routes
 import Network.HTTP.Types
 
-import Control.Monad.State
+import Control.Applicative (Applicative, (<*>), pure)
 
-import Control.Applicative (Applicative)
+-- Free Monad
+newtype F f a = F { runF :: forall r. (a -> r) -> (f r -> r) -> r }
+instance Functor f => Functor (F f) where
+  fmap f (F g) = F (\kp -> g (kp . f))
+instance Functor f => Applicative (F f) where
+  pure a = F (\kp _ -> kp a)
+  F f <*> F g = F (\kp kf -> f (\a -> g (kp . a) kf) kf)
+instance Functor f => Monad (F f) where
+  return a = F (\kp _ -> kp a)
+  F m >>= f = F (\kp kf -> m (\a -> runF (f a) kp kf) kf)
 
-data RouteState = RouteState
-                { middlewares :: [Middleware]
-                , defaultApp  :: Application
-                }
+-- | Add a layer
+wrap :: Functor f => f (F f a) -> F f a
+wrap f = F (\kp kf -> kf (fmap (\ (F m) -> m kp kf) f))
 
--- The final "catchall" application, simply returns a 404 response
--- Ideally you should put your own default application
-defaultApplication :: Application
-defaultApplication _req h = h $ responseLBS status404 [("Content-Type", "text/plain")] "Error : 404 - Document not found"
+-- | A version of lift that can be used with just a Functor for f.
+liftF :: Functor f => f a -> F f a
+liftF = wrap . fmap return
+-- End free monad things
 
+-- A Router functor can either add a middleware, or resolve to an app itself.
+data RouterF x = M Middleware x | D Application deriving Functor
 
-addMiddleware :: Middleware -> RouteState -> RouteState
-addMiddleware m s@(RouteState {middlewares=ms}) = s {middlewares=m:ms}
+-- Router type
+type RouteM = F RouterF
 
-setDefaultApp :: Application -> RouteState -> RouteState
-setDefaultApp a s = s {defaultApp=a}
+-- | Catch all routes and process them with the supplied application.
+-- Note: As expected from the name, no request proceeds past a catchall.
+catchall :: Application -> RouteM ()
+catchall a = liftF $ D a
 
--- | The Route Monad
-newtype RouteM a = S { runS :: StateT RouteState IO a }
-    deriving (Applicative, Monad, MonadIO, Functor, MonadState RouteState)
+-- | Synonym of `catchall`. Kept for backwards compatibility
+defaultAction :: Application -> RouteM ()
+defaultAction = catchall
 
--- | Add a middleware to the application.
--- Middleware is nested so the one declared earlier is outer.
+-- | Add a middleware to the application
+-- Middleware are ordered so the one declared earlier is wraps the remaining application.
 middleware :: Middleware -> RouteM ()
-middleware = modify . addMiddleware
+middleware m = liftF $ M m ()
 
 -- | Add a route to the application.
 -- Routes are ordered so the one declared earlier is matched first.
 route :: (Routable master master) => master -> RouteM ()
 route = middleware . routeDispatch
 
--- | Set the default action of the Application.
--- You should only call this once in an application.
--- Subsequent invocations override the previous settings.
-defaultAction :: Application -> RouteM ()
-defaultAction = modify . setDefaultApp
+-- The final "catchall" application, simply returns a 404 response
+-- Ideally you should put your own default application
+defaultApplication :: Application
+defaultApplication _req h = h $ responseLBS status404 [("Content-Type", "text/plain")] "Error : 404 - Document not found"
 
--- Empty state
-initRouteState :: RouteState
-initRouteState = RouteState [] defaultApplication
+-- | Convert a RouteM monad into a wai application.
+-- Note: We ignore the return type of the monad
+waiApp :: RouteM () -> Application
+waiApp (F r) = r (const defaultApplication) f
+  where
+    f (M m r) = m r
+    f (D a) = a
 
--- | Convert a RouteM Monadic value into a wai application.
-toWaiApp :: RouteM () -> IO Application
-toWaiApp m = do
-  (_,s) <- runStateT (runS m) initRouteState
-  return $ foldl (\a b -> b a) (defaultApp s) (middlewares s)
-
+-- | Similar to waiApp but returns the app in an arbitrary monad
+-- Kept for backwards compatibility
+toWaiApp :: Monad m => RouteM () -> m Application
+toWaiApp = return . waiApp
