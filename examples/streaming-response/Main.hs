b7dcd1347e6b8cc76f4d7127cb31101ae7a64cd5
Anupam Jain <ajnsit@gmail.com>
2015-08-08 22:08:53 +0530

    Fixed streaming response example

--- examples/streaming-response/Main.hs
+++ examples/streaming-response/Main.hs
@@ -1,14 +1,21 @@
 {-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, TypeFamilies, ViewPatterns, MultiParamTypeClasses, FlexibleInstances #-}
 module Main where
 {-
-  A demonstration of streaming response body
+  A demonstration of streaming response body.
+  Note: Most browsers will NOT display the streaming contents as is.
+  Try CURL to see the effect of streaming. "curl localhost:8080"
 -}
 
 import Network.Wai.Middleware.Routes
 import Network.Wai.Application.Static
 import Network.Wai.Handler.Warp
-import qualified Data.Text.Lazy as T
 import Network.Wai.Middleware.RequestLogger
+
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import Control.Concurrent (threadDelay)
+
+import Data.ByteString.Builder (intDec)
 
 -- The Master Site argument
 data MyRoute = MyRoute
@@ -22,20 +29,15 @@ mkRoute "MyRoute" [parseRoutes|
 
 -- Homepage
 getHomeR :: Handler MyRoute
-getHomeR = runHandlerM $ do
-  Just r <- maybeRoute
-  html $ T.concat
-    [ "<h1>Home</h1>"
-    , "<p>You are on route - "
-    ,   T.fromStrict $ showRoute r
-    , "</p>"
-    , "<p>"
-    ,   "<a href=\""
-    ,   T.fromStrict $ showRoute HelloR
-    ,   "\">Go to hello</a>"
-    ,   " to be greeted!"
-    , "</p>"
-    ]
+getHomeR = runHandlerM $ stream $ \write flush -> do
+    write "Starting Countdown\n"
+    flush
+    forM_ (reverse [1..10]) $ \n -> do
+      liftIO $ threadDelay 1000000
+      write $ intDec n
+      write "\n"
+      flush
+    write "Done!\n"
 
 -- The application that uses our route
 -- NOTE: We use the Route Monad to simplify routing
