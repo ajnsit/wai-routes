a50df27f3d7847d45851f63a4088347c4df8d161
Anupam Jain <ajnsit@gmail.com>
2015-11-25 12:39:05 +0530

    Derive Eq, Show, Ord for DefaultMaster and DefaultRoute

--- src/Network/Wai/Middleware/Routes/DefaultRoute.hs
+++ src/Network/Wai/Middleware/Routes/DefaultRoute.hs
@@ -23,11 +23,11 @@ import Data.Set (empty)
 import Network.Wai.Middleware.Routes.Routes
 
 -- Default master datatype, which is used for "unrouted" handlers
-data DefaultMaster = DefaultMaster
+data DefaultMaster = DefaultMaster deriving (Eq, Show, Ord)
 -- This makes it possible to define handlers without routing stuff
 instance RenderRoute DefaultMaster where
   -- The associated route simply contains all path information
-  data Route DefaultMaster = DefaultRoute ([Text],[(Text, Text)]) deriving Eq
+  data Route DefaultMaster = DefaultRoute ([Text],[(Text, Text)]) deriving (Eq, Show, Ord)
   renderRoute (DefaultRoute r) = r
 instance ParseRoute DefaultMaster where
   parseRoute = Just . DefaultRoute
